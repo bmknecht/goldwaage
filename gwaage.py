5056ee6aa22620e4b637696438741d049a096ef4
Birol Knecht <knecht.birol@gmail.com>
2013-02-18 15:21:35 +0100

    count appearances of all words, list words

--- gwaage.py
+++ gwaage.py
@@ -3,27 +3,49 @@ import codecs
 import sys
 import argparse
 
-def analyzeword(text, word, wlen):
-	words = text.split()
-	l = []
-	for i in xrange(wlen+len(words)):
-		l.append(words[max(i-wlen,0):i].count(word))
+def collectwords(text):
+	return set(text.split())
+	
+def countwords(text):
+	dc = {}
+	for word in text:
+		if word in dc:
+			dc[word] += 1
+		else:
+			dc[word] = 1
+	
+	return dc
 
-	return l
+def analyzetext(text, wlen):
+	splittext = text.split()
+	wdict = { word:[] for word in collectwords(text) }
+
+	for i in xrange(wlen+len(splittext)):
+		window = splittext[max(i-wlen,0):i]
+		for w, d in wdict.items():
+			d.append(window.count(w))
+	return wdict
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(
 	        description='zaehlt vorkomnisse des wortes')
 	parser.add_argument('textfile')
-	parser.add_argument('word')
-	parser.add_argument('-w', '--wlength', default=1, type=int)
+	parser.add_argument('-w', '--word')
+	parser.add_argument('-l', '--wlength', default=1, type=int)
 	args = parser.parse_args()
 	
-	l = analyzeword(
-		codecs.open(args.textfile, encoding='utf-8').read(),
-		args.word,
-		args.wlength)
+	text = codecs.open(args.textfile, encoding='utf-8').read()
+	if args.word:
+		wdict = analyzetext(
+				text,
+				args.wlength)
 
-	for v in l:
-		print('#'*v)
+		for v in wdict[args.word]:
+			print('#'*v)
+	else:
+		words = collectwords(text)
+		
+		for w in sorted(words):
+			print(w) 
+		
 
